6c809761d5a2f08044d2b7b6473794a681c1e12c
Arvindh Manian <arvindh.manian@gmail.com>
2025-02-15 15:33:05 -0800

    Add smart contract wrapper code

--- contracts/contracts/Donater.sol
+++ contracts/contracts/Donater.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 
 contract Donater is Ownable {
 
@@ -24,6 +23,9 @@ contract Donater is Ownable {
 
     event Donated(address indexed _user, uint256 _amount);
     event SplitAmongCharities(address indexed _user, uint256 _amount);
+    event CharitiesUpdated(address indexed _user, address[] _charities, uint256[] _charityPercents);
+    event Enrolled(address indexed _user, string[] _topics, address[] _charities, uint256[] _charityPercents);
+    event Withdrawn(address indexed _user, uint256 _amount);
 
     function enroll(string[] memory _topics, address[] memory _charities, uint256[] memory _charityPercents) public {
         require(_topics.length == 3, "Topics must be 3");
@@ -38,10 +40,11 @@ contract Donater is Ownable {
             charityPercents: _charityPercents,
             balance: 0
         });
+        emit Enrolled(msg.sender, _topics, _charities, _charityPercents);
         // console.log("Enrollment complete. First charity: %s, Percentage: %d", userTopic.charities[0], userTopic.charityPercents[0]);
     }
 
-    function changeTopics(address user, string[] memory _topics) public onlyUserOrOwner(user) {
+    function setTopics(address user, string[] memory _topics) public onlyUserOrOwner(user) {
         require(_topics.length == 3, "Topics must be 3");
         
         UserTopics storage userTopic = topics[user];
@@ -59,27 +62,22 @@ contract Donater is Ownable {
         require(topics[msg.sender].balance > 0, "No balance to withdraw");
 
         // send money back to msg.sender
-        payable(msg.sender).transfer(topics[msg.sender].balance);
+        uint256 amount = topics[msg.sender].balance;
         topics[msg.sender].balance = 0;
-    }
-
-    function updateProportion(address user, address charity, uint256 percentage) public onlyOwner {
-        for (uint256 i; i < topics[user].charityPercents.length; i++) {
-            if (topics[user].charities[i] == charity) {
-                topics[user].charityPercents[i] = percentage;
-                return;
-            }
-        }
-        require(false, "Charity not enrolled");
+        payable(msg.sender).transfer(amount);
+        emit Withdrawn(msg.sender, amount);
     }
 
     function getTopics(address user) public view returns (string[] memory _topics) {
         return topics[user].ttopics;    
     }
 
-    function addCharity(address user, address charity, uint256 percentage) public onlyOwner {
-        topics[user].charities.push(charity);
-        topics[user].charityPercents.push(percentage);
+    function setCharities(address user, address[] memory charities, uint256[] memory percentages) public onlyOwner {
+        require(charities.length == percentages.length, "Charities and percentages must be the same length");
+
+        topics[user].charities = charities;
+        topics[user].charityPercents = percentages;
+        emit CharitiesUpdated(user, charities, percentages);
     }
 
     function splitAmongCharities(address user) public onlyOwner {
