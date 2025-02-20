a2f66efee29133241ba5c597de5ed7ab570f32b2
Arvindh Manian <arvindh.manian@gmail.com>
2025-02-19 21:23:40 -0500

    feat: push updated crud logic to main

--- pg_module/models.py
+++ pg_module/models.py
@@ -1,18 +1,20 @@
-from sqlalchemy import Column, Text, ForeignKey, String
+from sqlalchemy import Column, Text, ForeignKey, String, Boolean, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mysql import VARCHAR
+
 
 Base = declarative_base()
 
 class CharityCategory(Base):
     __tablename__ = 'charitycategory'
 
-    category = Column(Text, nullable=False, index=True, primary_key=True)
-    charityname = Column(Text, nullable=False)
+    category = Column(Text, nullable=False, primary_key=True)
+    charityname = Column(Text, nullable=False, primary_key=True)
 
 class UserCategory(Base):
     __tablename__ = 'usercategory'
 
-    category = Column(Text, nullable=False, index=True, primary_key=True)
+    category = Column(Text, nullable=False, primary_key=True)
     userid = Column(Text, nullable=False, primary_key=True)
 
 class Charity(Base):
@@ -21,3 +23,24 @@ class Charity(Base):
     name = Column(String(255), nullable=False, unique=True, primary_key=True)
     mission = Column(Text)
     url = Column(String(2083))
+
+class UserPreferences(Base):
+    __tablename__ = 'userpreferences'
+
+    userid = Column(VARCHAR(100), primary_key=True)
+    mission_statement = Column(Text)
+    push_notifications = Column(Boolean, default=False)
+    prioritize_current_events   = Column(Boolean, default=False)
+
+class Counter(Base):
+    __tablename__ = 'counter'
+
+    userid = Column(VARCHAR(255), primary_key=True)
+    countvalue = Column(Integer)
+
+class CharityAddress(Base):
+    __tablename__ = 'charity_address'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    address = Column(String(100), nullable=False)
